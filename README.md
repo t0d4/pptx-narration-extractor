dd0ba878aa4c172f5039d4c0b1a9eafd3c3b5d33
Takaaki Toda <todatakaaki@gmail.com>
2021-10-23 12:35:13 +0900

    Completed command line tool

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Extract narrations and save into single file
 ## Requirements
+
+- pydub
+- tqdm
+
+Please install them by executing:
+`pip install -r requirements.txt`
+
 ## Usage
+
+python extractor.py [OPTIONS] PPTX_FILE
+
+Options:
+ --speed : the desired speed of the output audio (ex: 1.2) **Note: any value lower than 1.0 is currently not supported.**
--- extractor.py
+++ extractor.py
@@ -1,16 +1,20 @@
+import argparse
+import datetime
+import glob
 import os
 import pathlib
 import shutil
-import glob
 import sys
 import xml.etree.ElementTree as ET
-import argparse
 import zipfile
+
 from pydub import AudioSegment
+from pydub import effects
+from tqdm import tqdm
 
 TMP_DIR = pathlib.Path("tmp/")
 MEDIA_DIR = os.path.join(TMP_DIR, pathlib.Path("ppt/media"))
-TRANSITION_BEEP_SOUND = pathlib.Path("beep.m4a")
+TRANSITION_SOUND_DIR = pathlib.Path("beeps/")
 
 def get_extension(filename: str) -> str:
     return filename.split(".")[-1]
@@ -27,6 +31,11 @@ def main() -> None:
     args = parser.parse_args()
     pptx_filename = args.filename
     desired_speed = args.speed
+
+    if desired_speed is not None:
+        if desired_speed < 1.0:
+            print("values lower than 1.0 is not supported for parameter \"speed\".")
+            sys.exit(0)
 
     if not os.path.exists(pptx_filename):
         print("Couldn't find such pptx file.")
@@ -54,14 +63,44 @@ def main() -> None:
                 media_filename = os.path.basename(media_filepath)
                 extension = get_extension(media_filename)
                 if extension in ("m4a", "wav", "wma") and media_filename not in audio_filenames_in_the_slide:
+                    audio_extension = extension
                     audio_filenames_in_the_slide.append(media_filename)
         audio_filenames_in_the_file.append(audio_filenames_in_the_slide)
 
-    print(audio_filenames_in_the_file)
+    TRANSITION_BEEP_SOUND = os.path.join(TRANSITION_SOUND_DIR, f'beep.{audio_extension}')
 
+    basetime = datetime.datetime(year=2021, month=1, day=1, hour=0, minute=0, second=0)  # year, month and day are dummy value
+    elapsed_second = 0
+    merged_audio = None
+    with open("chapters.txt", "w+") as chapter_file:
+        for slide_idx, audio_filenames_in_the_slide in enumerate(tqdm(audio_filenames_in_the_file, "Processing audio files")):
+            if audio_filenames_in_the_slide == []:
+                continue
 
+            elapsed_time_formatted = (basetime+datetime.timedelta(seconds=elapsed_second)).strftime("%H:%M:%S")
+            chapter_file.write(f'{elapsed_time_formatted} slide{slide_idx+1}\n')
 
-    #shutil.rmtree(TMP_DIR)
+            for audio_filename in audio_filenames_in_the_slide:
+                audio = AudioSegment.from_file(os.path.join(MEDIA_DIR, audio_filename))
+                if desired_speed is not None:
+                    audio = audio.speedup(playback_speed=desired_speed, chunk_size=150, crossfade=25)
+                
+                # When process the first audio file 
+                if merged_audio is None:
+                    transition_sound_filename = os.path.join(TRANSITION_SOUND_DIR, f'beep.{get_extension(audio_filename)}')
+                    transition_sound = AudioSegment.from_file(transition_sound_filename)
+                    transition_sound = match_audio_volume(modulated_sound=transition_sound, base_sound=audio)
+                    merged_audio = audio
+                    elapsed_second += audio.duration_seconds
+                    continue
+
+                appended_sound = transition_sound + audio
+                merged_audio += appended_sound
+                elapsed_second += appended_sound.duration_seconds
+
+            merged_audio.export("narration.mp3", format="mp3", parameters=["-ac","2","-ar","8000"])
+
+    shutil.rmtree(TMP_DIR)
 
 if __name__ == '__main__':
     main()
